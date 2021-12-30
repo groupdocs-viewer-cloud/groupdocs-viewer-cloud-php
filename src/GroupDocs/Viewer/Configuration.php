7adca367359bc5d42a9dda39fec9586535d56a69
product-team <product.team@groupdocs.com>
2021-12-30 02:50:02 -0600

    Updated sources

--- src/GroupDocs/Viewer/Configuration.php
+++ src/GroupDocs/Viewer/Configuration.php
@@ -95,7 +95,7 @@ class Configuration
      * Version of client SDK
      *
      */
-    protected $clientVersion = '21.10';
+    protected $clientVersion = '21.12';
 
     /*
      * Constructor
@@ -290,7 +290,7 @@ class Configuration
     }
 
     /*
-     * Gets client version, default value is '21.10'
+     * Gets client version, default value is '21.12'
      *
      */
     public function getClientVersion()
@@ -308,7 +308,7 @@ class Configuration
         $report  = 'PHP SDK (GroupDocs\Viewer) Debug Report:' . PHP_EOL;
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
-        $report .= '    SDK Package Version: 21.10' . PHP_EOL;
+        $report .= '    SDK Package Version: 21.12' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;
